a3809d507c37eef4ebf1230ad439adddf319426a
vikash <vikash.mrdevops@gmail.com>
2025-07-14 15:06:06 +0530

    shared libs

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -47,7 +47,7 @@ def call(Map config = [:]) {
                 git commit -m "Update image tags to ${imageTag} and ensure correct domain [ci skip]"
                 
                 # Set up credentials for push
-                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/lax66/tws-e-commerce-app_hackathon.git
+                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/vikash-kumar01/e-commerce-app.git
                 git push origin HEAD:\${GIT_BRANCH}
             fi
         """
